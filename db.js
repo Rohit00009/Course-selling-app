66778f5ffdf38423aa34965b66721907e065e0c6
Rohit00009 <132641794+Rohit00009@users.noreply.github.com>
2025-07-02 00:53:23 +0530

    step7, only start when database is up

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-console.log("connected to database");
+// console.log("connected to database");
 mongoose.connect(
   "mongodb+srv://admin:5hURWwyBDvxuTikf@cluster0.cm424xg.mongodb.net/coursera-app"
 );
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 // const jwt = require("jsonwebtoken");
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 // const JWT_SECRET = "random123";
 const { courseRouter } = require("./routes/course");
 const { userRouter } = require("./routes/user");
@@ -13,4 +13,13 @@ app.use("/api/v1/user", userRouter);
 app.use("/api/v1/course", courseRouter);
 app.use("/api/v1/admin", adminRouter);
 
-app.listen(3000);
+async function main() {
+  await mongoose.connect(
+    "mongodb+srv://admin:5hURWwyBDvxuTikf@cluster0.cm424xg.mongodb.net/coursera-app"
+  );
+
+  app.listen(3000);
+  console.log("listening on port 3000");
+}
+
+main();
